311db0b2c617940de5e5015599da05dccc93239f
GarfieldMinusGarfield <nohlgriffin@gmail.com>
2025-10-24 03:37:46 -0600

    Added Homework
    Added files for homework

---
+++ Homework 9/sketch.js
@@ -0,0 +1,54 @@
+function setup()
+{
+    createCanvas(600, 430);
+}
+
+function draw()
+    {
+        background(153, 0, 61);
+        fill(255, 153, 51)
+        strokeWeight(3)
+        circle(300, 100, 150)
+        strokeWeight(25);
+        point(264, 90)
+        point(332, 90)
+        strokeWeight(4)
+        fill(255, 140, 26);
+        triangle(294, 110, 297, 100, 300, 110)
+        strokeWeight(3)
+        line(252, 70, 275, 65)
+        line(320, 74, 343, 74)
+
+        fill(0, 45, 179)
+        rect(238, 176, 120, 250)
+
+        fill(300, 100, 150)
+        ellipse(380, 380, 60, 33)
+        ellipse(214, 380, 60, 33)
+
+        fill(255, 255, 102)
+        square(25,14,5)
+        square(90,300,5)
+        square(72,170,5)
+        square(390,132,5)
+        square(120,50,5)
+        square(43,75,5)
+        square(100,130,5)
+        square(200,160,5)
+        square(173,250,5)
+        square(160,114,5)
+        square(84,16,5)
+        square(548,22,5)
+        square(507,37,5)
+        square(410,145,5)
+        square(462,100,5)
+        square(370,208,5)
+        square(559,200,5)
+        square(587,368,5)
+        square(520,425,5)
+
+        textFont('Snell')
+        textSize(14)
+        text("Ce n'est pas une personne", 20, 400)
+    }
+
